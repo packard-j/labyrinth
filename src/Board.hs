76629695565555065835158a711e1144e631b319
packard-j <packard.ja@husky.neu.edu>
2022-12-27 17:52:01 -0500

    add sliding, reformat tests

--- src/Board.hs
+++ src/Board.hs
@@ -1,19 +1,26 @@
-module Board (newBoard, reachableTiles, tileAtSafe) where
+module Board (newBoard, slide, reachableTiles, tileAtSafe) where
 import Tile (Tile(..), tilesConnected)
 import Coordinate (Coordinate(..), add)
 import Orientation (Orientation(..), toUnitVector)
 import Data.Graph (Graph, Vertex, graphFromEdges, reachable)
-import Data.Set (Set, fromList)
+import Data.Set (Set, fromList, empty, member)
+import Prelude hiding (Either(..))
 
 data Board = Board 
   { tiles  :: [[Tile]],
     width  :: Integer,
-    height :: Integer }
+    height :: Integer,
+    movable :: Set (Axis, Integer) }
 
 instance Show Board where
    show board = 
      show (width board) ++ "x" ++ show (height board) ++ " board" ++ "\n" ++
      unlines (map (unwords . map show) (tiles board))
+
+data Axis = Row | Column deriving (Eq, Ord)
+
+data UpOrDown = Up | Down
+data LeftOrRight = Left | Right
 
 newBoard :: (Coordinate -> Tile) -> Integer -> Integer -> Board
 newBoard createTile w h =
@@ -21,6 +28,48 @@ newBoard createTile w h =
     (map (\row -> boardRow createTile row w) [0..h-1])
     w
     h
+    empty
+
+slide :: Board -> Tile -> Orientation -> Integer -> Maybe (Board, Tile)
+slide board spare North = slideCol board spare Up
+slide board spare South = slideCol board spare Down
+slide board spare East  = slideRow board spare Right
+slide board spare West  = slideRow board spare Left
+
+slideRow :: Board -> Tile -> LeftOrRight -> Integer -> Maybe (Board, Tile)
+slideRow board tile dir row
+  | row < 0 || row >= height board = Nothing
+  | not $ member (Row, row) (movable board) = Nothing
+  | otherwise = Just (shifted dir, newSpare dir) where
+    shifted  Left  = mapBoard board slideLeft
+    shifted  Right = mapBoard board slideRight
+    newSpare Left  = tileAt board $ Coordinate 0 row
+    newSpare Right = tileAt board $ Coordinate (width board - 1) row
+    slideLeft  (Coordinate x _)
+      | x == width board - 1 = tile
+      | otherwise = tileAt board $ Coordinate (x + 1) row
+    slideRight (Coordinate x _)
+      | x == 0 = tile
+      | otherwise = tileAt board $ Coordinate (x - 1) row
+
+slideCol :: Board -> Tile -> UpOrDown -> Integer -> Maybe (Board, Tile)
+slideCol board tile dir col
+  | col < 0 || col >= width board = Nothing
+  | not $ member (Column, col) (movable board) = Nothing
+  | otherwise = Just (shifted dir, newSpare dir) where
+    shifted  Up   = mapBoard board slideUp
+    shifted  Down = mapBoard board slideDown
+    newSpare Up   = tileAt board $ Coordinate col 0
+    newSpare Down = tileAt board $ Coordinate col (height board - 1)
+    slideUp   (Coordinate _ y)
+      | y == height board - 1 = tile
+      | otherwise = tileAt board $ Coordinate col (y + 1)
+    slideDown (Coordinate _ y)
+      | y == 0 = tile
+      | otherwise = tileAt board $ Coordinate col (y - 1)
+
+mapBoard :: Board -> (Coordinate -> Tile) -> Board
+mapBoard board f = newBoard f (width board) (height board)
 
 tileAtSafe :: Board -> Coordinate -> Maybe Tile
 tileAtSafe board coordinate
--- test/Spec.hs
+++ test/Spec.hs
@@ -9,63 +9,77 @@ import Data.Set (fromList)
 
 genTile :: Coordinate -> Tile
 genTile (Coordinate x y) =
-  Tile (connectors !! fromIntegral (x `mod` fromIntegral (length connectors)))
+  Tile (connectors   !! fromIntegral (x `mod` fromIntegral (length connectors)))
        (orientations !! fromIntegral (y `mod` fromIntegral (length orientations)))
   where
     connectors   = [Bar, L, T, Plus]
     orientations = [North, East, South, West]
 
-testAccess = TestCase $
-  assertEqual "tile at (0,0)" (tileAtSafe (newBoard genTile 3 3) (Coordinate 0 0)) (Just $ Tile Bar North)
+tileAtOrigin :: Test
+tileAtOrigin = 
+  Just (Tile Bar North) ~?= tileAtSafe (newBoard genTile 3 3) (Coordinate 0 0) 
 
-testConnected = TestCase $
-  assertBool "─┼ should be connected" (tilesConnected (Tile Plus North) (Tile Bar East) East)
+barPlusConnected :: Test
+barPlusConnected =
+  tilesConnected (Tile Plus North) (Tile Bar East) East ~? 
+  "─┼ should be connected" 
 
-testConnected2 = TestCase $
-  assertBool "┐\n└ should be connected" (tilesConnected (Tile L North) (Tile  L South) North)
+verticalLsConnected :: Test
+verticalLsConnected =
+  tilesConnected (Tile L North) (Tile  L South) North ~? 
+  "┐\n└ should be connected"
 
-testNotConnected = TestCase $
-  assertBool "└\n┐ should not be connected" (not (tilesConnected (Tile L North) (Tile L South) South))
+verticalLsDisconnected :: Test
+verticalLsDisconnected =
+  not (tilesConnected (Tile L North) (Tile L South) South) ~? 
+  "└\n┐ should not be connected"
 
-testConnected3 = TestCase $
-  assertBool "└─ should be connected" (tilesConnected (Tile Bar East) (Tile L North) West)
+lBarConnected :: Test
+lBarConnected = 
+  tilesConnected (Tile Bar East) (Tile L North) West ~?
+  "└─ should be connected"
 
-testNotConnected2 = TestCase $
-  assertBool "─└ should not be connected" (not (tilesConnected (Tile Bar East) (Tile L North) North))
+barLDisconnected :: Test
+barLDisconnected =
+  not (tilesConnected (Tile Bar East) (Tile L North) North) ~?
+  "─└ should not be connected"
 
-testConnectedHuh = TestCase $
-  assertBool "┐\n┴ should be connected" (tilesConnected (Tile L South) (Tile T South) South)
-
+lTConnected :: Test
+lTConnected =
+  tilesConnected (Tile L South) (Tile T South) South ~?
+  "┐\n┴ should be connected"
 
 -- │ ─ │
 -- └ ┌ ┐
 -- ┬ ┤ ┴
 
-testReachable = TestCase $
-  assertEqual "tile (0, 0)" 
-              (reachableTiles (newBoard genTile 3 3) (Coordinate 0 0))
-              (Just $ fromList [Coordinate 0 0, Coordinate 0 1])
+reachableFromOrigin :: Test
+reachableFromOrigin = 
+  Just (fromList [Coordinate 0 0, Coordinate 0 1]) ~?=
+  reachableTiles (newBoard genTile 3 3) (Coordinate 0 0)
 
-testReachable2 = TestCase $
-  assertEqual "tile (2, 2)"
-              (reachableTiles (newBoard genTile 3 3) (Coordinate 2 2))
-              (Just $ fromList [Coordinate 1 1, Coordinate 2 1, Coordinate 0 2, Coordinate 1 2, Coordinate 2 2])
+reachableFrom2_2 :: Test
+reachableFrom2_2 = 
+  Just (fromList [Coordinate 1 1, Coordinate 2 1, Coordinate 0 2, Coordinate 1 2, Coordinate 2 2]) ~?=
+  reachableTiles (newBoard genTile 3 3) (Coordinate 2 2)
 
-testReachable3 = TestCase $
-  assertEqual "tile (2, 1)"
-              (reachableTiles (newBoard genTile 3 3) (Coordinate 2 1))
-              (Just $ fromList [Coordinate 1 1, Coordinate 2 1, Coordinate 0 2, Coordinate 1 2, Coordinate 2 2]) 
+reachableFrom2_1 :: Test
+reachableFrom2_1 =
+  Just (fromList [Coordinate 1 1, Coordinate 2 1, Coordinate 0 2, Coordinate 1 2, Coordinate 2 2]) ~?= 
+  reachableTiles (newBoard genTile 3 3) (Coordinate 2 1)
 
+tests :: Test
 tests = TestList
-  [ TestLabel "board access" testAccess,
-    TestLabel "connected" testConnected,
-    TestLabel "connected 2" testConnected2,
-    TestLabel "not connected" testNotConnected,
-    TestLabel "connected 3" testConnected3,
-    TestLabel "not connected 2" testNotConnected2,
-    TestLabel "connected huh?" testConnectedHuh,
-    TestLabel "reachable" testReachable,
-    TestLabel "reachable more" testReachable2 ]
+  [ "tile at (0,0)"        ~: tileAtOrigin,
+    "─┼ connected"         ~: barPlusConnected,
+    "┐\n└ connected"       ~: verticalLsConnected,
+    "└\n┐ disconnected"    ~: verticalLsDisconnected,
+    "└─ connected"         ~: lBarConnected,
+    "─└ disconnected"      ~: barLDisconnected,
+    "┐\n┴ connected"       ~: lTConnected,
+    "reachable from (0,0)" ~: reachableFromOrigin,
+    "reachable from (2,1)" ~: reachableFrom2_2,
+    "reachable from (2,1)" ~: reachableFrom2_1]
 
 main :: IO ()
 main = do
