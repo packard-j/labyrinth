379127168308594be53073e432cc672d08e7865d
packard-j <packard.ja@husky.neu.edu>
2022-12-30 23:02:37 -0500

    state implementation

---
+++ src/State.hs
@@ -0,0 +1,99 @@
+module State (State, PlayerPieces(..), newState, newStateWithSlide, move) where
+import Board
+import Tile
+import Coordinate
+import Orientation
+
+-- | Represents the state of a game of labyrinth, including the
+-- | game board, the current spare tile, the last slide action
+-- | that occurred, and the list of players in the game. 
+data State p = State
+  { board :: Board,
+    -- | The most recent slide action that has been performed by
+    -- | a player, if one has occured yet. Otherwise, Nothing.
+    lastSlide :: Maybe (Orientation, Integer),
+    -- | The current spare tile that will be used on the next
+    -- | slide action.
+    spare :: Tile,
+    -- | The list of players and their respective home, goal, and
+    -- | position coordinates.
+    -- | The player that is next to take a turn is at the head of
+    -- | the list of players, and the remaining players follow in
+    -- | turn-order.
+    players :: [PlayerPieces p] } deriving (Show, Eq)
+
+-- | Represents the player in a game of labyrinth and their
+-- | respective home, goal, and current position on the board.
+data PlayerPieces p = PlayerPieces
+  { home     :: Coordinate,
+    goal     :: Coordinate,
+    position :: Coordinate,
+    player   :: p } deriving (Show, Eq)
+
+-- | Construct the state for a new game of labyrinth to be played on
+-- | the given board, with the specified spare tile and players.
+-- | The player pieces must be located on the board, and each player's
+-- | home and goal must be located on fixed tiles.
+newState :: Board -> Tile -> [PlayerPieces p] -> Maybe (State p)
+newState = newStateWithSlide Nothing
+
+-- | Construct a state with the specified previous slide action.
+newStateWithSlide :: Maybe (Orientation, Integer) -> Board -> Tile -> [PlayerPieces p] -> Maybe (State p)
+newStateWithSlide prevSlide gameBoard spareTile playerPieces
+  | not $ all (isOnBoard gameBoard) coords = Nothing
+  | not $ all (isOnFixedTile gameBoard) homesAndGoals = Nothing
+  | otherwise = Just $ State gameBoard prevSlide spareTile playerPieces
+    where coords = position <$> playerPieces
+          homesAndGoals = concatMap (\p -> [home p, goal p]) playerPieces
+
+-- | Perform a slide and move action as the current player in the turn-order.
+-- | The slide is specified by the orientation to slide towards and the index
+-- | of the axis. The destination to move the current player to is specified
+-- | by the coordinate.
+-- | Nothing is returned if:
+-- |   * the slide cannot be performed
+-- |   * the player cannot move to the specified destination after the slide
+-- |   * the specified destination is the same as the player's position after
+-- |     the slide.
+move :: State p -> Orientation -> Integer -> Coordinate -> Maybe (State p)
+move state dir axis to 
+  | lastSlide state == Just (rotateClockwiseBy dir South, axis) = Nothing
+  | otherwise = do
+    (shiftedBoard, nextSpare) <- slide (board state) (spare state) dir axis
+    nextPlayers <- updatePlayers shiftedBoard (players state) dir axis to
+    return $ State shiftedBoard (Just (dir, axis)) nextSpare (rotate nextPlayers)
+
+-- | Update the player positions in response to a slide and move action.
+-- | All players on the affected row or column will be shifted, then the current
+-- | player will be moved to the destination, if possible.
+-- | Nothing is returned if:
+-- |   * the destination tile cannot be reached from the player's position
+-- |     after sliding.
+-- |   * the player is already at the destination after the slide.
+updatePlayers :: Board -> [PlayerPieces p] -> Orientation -> Integer -> Coordinate -> Maybe [PlayerPieces p]
+updatePlayers _ [] _ _ _ = Just []
+updatePlayers brd (p0:rest) dir axis to = do
+  moved <- movePlayer brd playerAfterShift to
+  if position moved == position playerAfterShift
+     then Nothing
+     else Just $ moved:tail shifted
+  where
+    shifted = shiftPlayer brd dir axis <$> p0:rest
+    playerAfterShift = head shifted
+
+-- | Shift a player's position along with a slide action on a board, if it was affected.
+shiftPlayer :: Board -> Orientation -> Integer -> PlayerPieces p -> PlayerPieces p
+shiftPlayer b dir axis p = p 
+  { position = shiftAlong b dir axis (position p) }
+
+-- | Move a player to the specified coordinate on the board, if possible.
+movePlayer :: Board -> PlayerPieces p -> Coordinate -> Maybe (PlayerPieces p)
+movePlayer b pieces to = do
+  canMove <- pathExists b (position pieces) to
+  if canMove then return moved else Nothing where
+    moved = pieces { position = to }
+
+-- | Rotate a list by moving the item at the head of the list to the back.
+rotate :: [a] -> [a]
+rotate (x:xs) = xs ++ [x]
+rotate empty  = empty
---
+++ test/StateTest.hs
@@ -0,0 +1,43 @@
+module StateTest (stateTests) where
+import Test.HUnit
+import Examples.State
+import Examples.Board
+import State
+import Coordinate
+import Orientation
+
+state3x3Move :: Test
+state3x3Move = expected ~=? actual where
+  actual = do
+    state <- state3x3
+    move state North 0 $ Coordinate 0 1 
+  expected = newStateWithSlide (Just (North, 0)) board3x3ShiftCol0U (tile '│') expectedPlayers
+  expectedPlayers =
+    [ PlayerPieces (Coordinate 1 1)
+                   (Coordinate 1 1)
+                   (Coordinate 2 1)
+                   'b',
+      PlayerPieces (Coordinate 1 1)
+                   (Coordinate 1 1)
+                   (Coordinate 0 1)
+                   'a' ]
+
+state3x3InvalidHome :: Test
+state3x3InvalidHome = Nothing ~=? state where
+  state = newState board3x3 (tile '│') players
+  players = [ PlayerPieces (Coordinate 0 1)
+                           (Coordinate 1 1)
+                           (Coordinate 2 1)
+                           'a' ]
+
+state3x3Unreachable :: Test
+state3x3Unreachable = Nothing ~=? invalidMove where
+  invalidMove = do
+    state <- state3x3
+    move state North 0 $ Coordinate 0 0
+
+stateTests :: Test
+stateTests = TestList 
+  [ "move player a to (0,1)" ~: state3x3Move,
+    "cannot place a home on a movable tile" ~: state3x3InvalidHome,
+    "cannot move player a to (0,0)" ~: state3x3Unreachable ]
